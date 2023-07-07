7f195f5a81a947a3018f24856c45d4b2116eaa45
Eric <849895374@qq.com>
2023-07-07 13:36:12 +1000

    delete github auth

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -37,7 +37,7 @@ const AuthModal = () => {
       <Auth
         theme="dark"
         magicLink
-        providers={["github"]}
+        providers={[]}
         supabaseClient={supabaseClient}
         appearance={{
           theme: ThemeSupa,
