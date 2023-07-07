4034a0df049431be08e47972955fc7794cd02c5d
Eric <849895374@qq.com>
2023-07-07 13:27:56 +1000

    fix redirect bugs

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -3,7 +3,7 @@ import { Price } from "@/types"
 export const getURL = () => {
   let url = 
     process.env.NEXT_PUBLIC_SITE_URL ?? 
-    process.env.MEXT_PUBLIC_VERCEL_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
     'http://localhost:3000/'
 
   url = url.includes('http') ? url : `http://${url}`
